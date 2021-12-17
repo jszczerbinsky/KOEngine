424950aac8e52d7a7d3cf3ec801613847b2d097d
jszczerbinsky <j.bek.s212@gmail.com>
2021-12-17 14:47:15 +0100

    Added Lerp function

--- utils.c
+++ utils.c
@@ -4,6 +4,18 @@
 #include "KOEngine.h"
 #include "utils.h"
 
+Vector2D Lerp(Vector2D p1, Vector2D p2, float t)
+{
+	if(t < 0 ) t = 0;
+	else if(t > 1) t = 1;
+
+	Vector2D p;
+	p.x = p1.x + (p2.x - p1.x) * t;
+	p.y = p1.y + (p2.y - p1.y) * t;
+
+	return p;
+}
+
 float Distance(Vector2D p1, Vector2D p2)
 {
 	return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y,2 ));
--- utils.h
+++ utils.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+Vector2D Lerp(Vector2D p1, Vector2D p2, float t);
 float Distance(Vector2D p1, Vector2D p2);
 bool CheckInside(Entity *ent, Vector2D startPos, Vector2D endPos);
 
